e9deb3f9cdad05748868086029a404a27d4774f6
Oluwaseun Akinruli <42046366+ruliseun@users.noreply.github.com>
2023-03-29 21:03:41 +0100

    Enabled cors on server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv'
+import { Environment } from './common/enums';
 
 dotenv.config();
+
+const getAllowedOrigins = (environment: Environment) => {
+
+  if (environment === Environment.DEVELOPMENT) {
+    return ['http://localhost:3000'];
+  }
+
+  return '*';
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -15,6 +25,12 @@ async function bootstrap() {
   process.env.TZ = 'Africa/Lagos';
 
   app.setGlobalPrefix('api/v1');
+  
+  app.enableCors({
+    origin: getAllowedOrigins(environment),
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   await app.listen(port);
 
